93a16439ac9cea420d7e353e52e59e79a4f72de1
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-28 13:59:20 +0700

    app

--- server.js
+++ server.js
@@ -4,7 +4,4 @@ io.on('connection', socket => {
     socket.on('CLIENT_SEND_MESSAGE', message => {
         io.emit('SERVER_SEND_MESSAGE', 'You: ' + message);
     });
-    // setInterval(() => {
-    //     socket.emit('CLIENT_SEND_MESSAGE', Math.random());
-    // }, 1000);
 });
--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: []
+      messages: [],
+      users: [],
+      isLoggedIn: false
     }
   }
   componentDidMount() {
@@ -20,16 +22,33 @@ class App extends Component {
     socket.emit('CLIENT_SEND_MESSAGE', message);
   }
   render() {
-    return (
-      <div style={{ padding: '10px' }}>
-        <input placeholder="Message" ref="txtMessage" />
-        <br /><br />
-        <button onClick={this.onSendMessage.bind(this)}>
-          Send
-        </button>
-        { this.state.messages.map((e, index) => <p key={index}>{e}</p>) }
+    const { users, isLoggedIn, messages } = this.state;
+    if (isLoggedIn) return (
+      <div style={{ padding: '10px', display: 'flex', flexDirection: 'row' }}>
+        <div>
+          <input placeholder="Message" ref="txtMessage" />
+          <br /><br />
+          <button onClick={this.onSendMessage.bind(this)}>
+            Send
+          </button>
+          <h4>Messages</h4>
+          { messages.map((e, index) => <p key={index}>{e}</p>) }
+        </div>
+        <div style={{ padding: '10px', marginTop: 50 }}>
+            <h4>User Online</h4>
+            { users.map((e, index) => <p key={index}>{e}</p>) }
+        </div>
       </div>
     );
+    return (
+      <div style={{ padding: '10px' }}>
+        <input placeholder="Username" ref="txUsername" />
+        <br /><br />
+        <button onClick={this.onSendMessage.bind(this)}>
+          Sign In
+        </button>
+      </div>
+    )
   }
 }
 
